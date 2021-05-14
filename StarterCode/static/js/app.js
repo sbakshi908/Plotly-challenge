ef12941524a5d60ac8925d36451d9506c5ba1cb6
Summya Bakshi <summyabakshi@gmail.com>
2021-05-14 12:31:05 -0400

    update charts

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -17,42 +17,66 @@ d3.json("data/samples.json").then((samples)  =>{
 
 //first function for bar chart 
 
-function createBarchart(sample_id){
-    d3.json("data/samples.json").then( (samples_data) => {
+function createBarchart(sample_id) {
+    d3.json("data/samples.json").then((samples_data) => {
         var samples = samples_data.samples;
         var samples_array = samples.filter(obj => obj.id == sample_id);
         var results = samples_array[0];
-        
-        //declare vaiabekls for each element in dictionary
+        //console.log(`these are the results ${results}`);
+
+        //declare variables for each element in dictionary
         var otu_ids = results.otu_ids;
         var otu_labels = results.otu_labels;
         var sample_values = results.sample_values;
 
-        var yticks = otu_ids.slice(0,10).map(otuid => `OTU ${otuid}`).reverse();
+        var yticks = otu_ids.slice(0, 10).map(otuid => `OTU ${otuid}`).reverse();
         console.log(results);
 
         var trace1 = {
-            x: sample_values.slice(0,10).reverse(),
+            x: sample_values.slice(0, 10).reverse(),
             y: yticks,
             type: "bar",
             orientation: "h"
-
         };
-
         var layout = {
             title: "Top 10 Bacteria"
         };
-
         var bar_data = [trace1];
-        Plotly.newPlot("bar", bar_data, layout );
+        Plotly.newPlot("bar", bar_data, layout);
     });
     //console.log(`calling create bar chart with sameple id ${sample_id}`)
-
 };
 
 //bubble chart fucntion
 
 function createBubblechart(sample_id) {
+    d3.json("data/samples.json").then( (samples_data) => {
+        var samples = samples_data.samples;
+        var samples_array = samples.filter(obj => obj.id == sample_id);
+        var results = samples_array[0];
+
+        var otu_ids = results.otu_ids;
+        var otu_labels = results.otu_labels;
+        var sample_values = results.sample_values;
+
+        var yticks = sample_values
+
+        var trace2 ={
+            x: otu_ids,
+            y: yticks,
+            type: "bubble"
+        };
+
+        var layout = {
+            title: "Biodiversity bubble chart"
+        };
+        var bar_data = [trace2];
+        Plotly.newPlot("bubble", bar_data, layout);
+
+
+
+    });    
+   
     console.log(`calling create bubble chart with sample id ${sample_id}`)
 };
 
@@ -62,20 +86,3 @@ function optionChanged(new_id){
     createBarchart(new_id);
     createBubblechart(new_id);
 };
-// create trace 
-// var trace1 = {
-//     x: samples.sample_values.slice(0,9),
-//     y: samples.otu_ids/slice(0,9),
-//     type: "bar",
-//     orientation: "h"
-// };
-// var layOut = {
-//     title: "Top Ten Bacteria Found ",
-//     //xaxis: {},
-//     //yaxis: {}
-// };
-
-// });
-// Ploty.newPlot("trace1", l )
-
-// var bar_chart = d3.select("#bar").node()
